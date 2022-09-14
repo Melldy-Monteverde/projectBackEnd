d3d210d49195df6b721babe2e185ac4bc221defa
Melldy-Monteverde <102992135+Melldy-Monteverde@users.noreply.github.com>
2022-09-14 18:00:13 -0300

    desafio 1 backend

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('Hello word!')
+class User {
+  constructor(name, lastname, pets, books) {
+    this.name = name;
+    this.lastname = lastname;
+    this.pets = pets;
+    this.books = books;
+  }
+  getFullName() {
+    return `${this.name} ${this.lastname}`;
+  }
+  addPet(pet) {
+    this.pets.push(pet);
+  }
+  countPets() {
+    return this.pets.length;
+  }
+  addBook(name, author) {
+    this.books.push({
+      name: name,
+      author: author,
+    });
+  }
+  getBookNames() {
+    console.log('Libros leidos: ');
+    return this.books.map((book) => book.name);
+  }
+}
+
+let user = new User(
+  'Melldy',
+  'Monteverde',
+  [],
+  [{ name: 'La divina comedia', author: 'Dante Alighieri' }]
+);
+
+user.addPet('Chanler');
+user.addBook('Confesiones de un chef', 'Anthony Bourdain');
+console.log('Usuario:', user.getFullName());
+console.log('Tengo: ' + user.countPets() + ' gato');
+
+user.getBookNames().forEach((book) => {
+  console.log(book);
+});
