80b2aecd12d91a1dabd15ed9d99af4941c56d301
Melldy Monteverde <monteverdelex@gmail.com>
2022-09-17 15:12:24 -0300

    crea rama desafios

---
+++ desafios/desf_2.js
@@ -0,0 +1,13 @@
+class Container {
+  constructor(file) {
+    this.file = file
+  }
+
+  save(obj){} // return obj id
+  getById(id){} // return obj
+  getAll(){} // return obj[]
+  deleteById(id){} // delete something form obj[]
+  deleteAll(){} // delete all from obj[] 
+}
+
+console.log('still working')
